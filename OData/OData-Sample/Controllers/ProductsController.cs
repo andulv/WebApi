6180382f3bf473b4f8dbe3b7d2c5af52cdbeab51
Anders Ulveseth <anders@ulveseth.net>
2016-12-09 01:59:48 +0100

    Lagt til tre ulike måter å applye OData querystring på.

--- OData/OData-Sample/Controllers/ProductsController.cs
+++ OData/OData-Sample/Controllers/ProductsController.cs
@@ -5,28 +5,62 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.OData;
+using System.Web.OData.Builder;
 using System.Web.OData.Query;
+using System.Web.OData.Extensions;
 
 namespace OData_Sample.Controllers
 {
     public class ProductsController : ApiController
     {
-        // GET api/values
-        public IList<Product> Get1(ODataQueryOptions<Product> opts)
+
+        //Her er det mye automatikk.
+        //Vi returnerer en IQueryable<>, så kicker mediatypeformatter (eller noe lingnende) inn, lager ODataQueryOptions fra querystring og apply'er den før serializering.
+        [EnableQuery]
+        public IQueryable<Product> Get1()
+        {
+            Product[] products = GetProducts();
+            return products.AsQueryable();
+        }
+
+        //Litt mindre automatikk
+        //Vi spesifiserer at vi vil ha ODataQueryOptions som argument, en modelbinder sørger for at den blir opprettet fra querystring
+        public IList<Product> Get2(ODataQueryOptions<Product> opts)
         {
             var products = GetProducts().AsQueryable();
             opts.ApplyTo(products);
             return products.ToList();
         }
 
-        // GET api/values
-        [EnableQuery]
-        public IQueryable<Product> Get2()
+        //Her er manuell måte å hente ODataQueryOptions fra scratch.
+        public IList<Product> Get3()
         {
-            Product[] products = GetProducts();
-            return products.AsQueryable();
+            ODataQueryOptions<Product> opts = GetOdataQueryOptions<Product>();
+
+            var products = GetProducts().AsQueryable();
+            products = opts.ApplyTo(products) as IQueryable<Product>;
+            return products.ToList();
         }
 
+        private ODataQueryOptions<T> GetOdataQueryOptions<T>()
+        {
+            var entityClrType = typeof(T);
+            Microsoft.OData.Edm.IEdmModel edmModel = BuildModel(entityClrType);
+            var odataPath = this.Request.ODataProperties().Path;
+            var odataContext = new ODataQueryContext(edmModel, entityClrType, odataPath);
+            var opts = new ODataQueryOptions<T>(odataContext, this.Request);
+            return opts;
+        }
+
+        //NB! Tror denne er expensive å lage, og forandrer seg aldri. Bør caches.
+        private Microsoft.OData.Edm.IEdmModel BuildModel(Type entityClrType)
+        {
+            var builder = new ODataConventionModelBuilder(this.Configuration, isQueryCompositionMode: true);
+            var entityTypeConfiguration = builder.AddEntityType(entityClrType);
+            builder.AddEntitySet(entityClrType.Name, entityTypeConfiguration);
+            var edmModel = builder.GetEdmModel();
+            return edmModel;
+        }
 
         private static Product[] GetProducts()
         {
@@ -37,13 +71,6 @@ namespace OData_Sample.Controllers
                 new Product { Id=3, Name="Produkt 2", TypeCode=0},
             };
         }
-
-        // GET api/values/5
-        public Product Get(int id)
-        {
-            return GetProducts().SingleOrDefault(x => x.Id == id);
-        }
-
     }
 
     public class Product
